4c33bdfc84658fd7061c2a904aebbece2e44f0e5
Rachit Chahar <202151123@iiitvadodara.ac.in>
2025-01-02 15:17:02 +0530

    fix: add redirect

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,6 +4,7 @@ import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
 import Layout from "@theme/Layout";
 // import HomepageFeatures from '@site/src/components/HomepageFeatures';
 import Heading from "@theme/Heading";
+import { Redirect } from "@docusaurus/router";
 
 import styles from "./index.module.css";
 
@@ -71,11 +72,12 @@ function HomepageHeader() {
 export default function Home(): JSX.Element {
   const { siteConfig } = useDocusaurusContext();
   return (
-    <Layout
-      title={`Docs`}
-      description="Description will go into a meta tag in <head />"
-    >
-      <HomepageHeader />
-    </Layout>
+    // <Layout
+    //   title={`Docs`}
+    //   description="Description will go into a meta tag in <head />"
+    // >
+    //   <HomepageHeader />
+    // </Layout>
+    <Redirect to="/docs/intro" />
   );
 }
